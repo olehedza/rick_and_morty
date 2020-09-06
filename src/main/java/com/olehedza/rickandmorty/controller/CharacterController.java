28b223eb8937e15b3ae0c3d4c563e106f289d6eb
olehedza <BiewZpo2>
2020-09-07 00:56:54 +0300

    create rest controller

---
+++ src/main/java/com/olehedza/rickandmorty/controller/CharacterController.java
@@ -0,0 +1,38 @@
+package com.olehedza.rickandmorty.controller;
+
+import com.olehedza.rickandmorty.dto.response.CharacterResponseDto;
+import com.olehedza.rickandmorty.mapper.CharacterMapper;
+import com.olehedza.rickandmorty.model.Character;
+import com.olehedza.rickandmorty.service.CharacterService;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/character")
+public class CharacterController {
+    private final CharacterService characterService;
+    private final CharacterMapper characterMapper;
+
+    public CharacterController(CharacterService characterService,
+                               CharacterMapper characterMapper) {
+        this.characterService = characterService;
+        this.characterMapper = characterMapper;
+    }
+
+    @GetMapping("/random")
+    public CharacterResponseDto getRandomCharacter() {
+        Character randomById = characterService.getRandomById();
+        return characterMapper.toDto(randomById);
+    }
+
+    @GetMapping
+    public List<CharacterResponseDto> filterAllByNameString(@RequestParam String name) {
+        return characterService.getAllByNameContains(name).stream()
+                .map(characterMapper::toDto)
+                .collect(Collectors.toList());
+    }
+}
