6231ae367a7ba7900960b1ddad195aafb546b033
olehedza <BiewZpo2>
2020-09-10 02:50:27 +0300

    fix database synchronizer

--- src/main/java/com/olehedza/rickandmorty/util/ClientDatabaseStartupSynchronization.java
+++ src/main/java/com/olehedza/rickandmorty/util/ClientDatabaseStartupSynchronization.java
@@ -1,15 +1,14 @@
 package com.olehedza.rickandmorty.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.olehedza.rickandmorty.client.RickAndMortyAPIClient;
 import com.olehedza.rickandmorty.dto.client.CharacterDto;
-import com.olehedza.rickandmorty.dto.client.CharactersDto;
 import com.olehedza.rickandmorty.mapper.CharacterMapper;
 import com.olehedza.rickandmorty.mapper.LocationMapper;
 import com.olehedza.rickandmorty.mapper.OriginMapper;
 import com.olehedza.rickandmorty.model.Character;
 import com.olehedza.rickandmorty.service.CharacterService;
 import com.olehedza.rickandmorty.service.EpisodeService;
-import com.olehedza.rickandmorty.service.JsonParser;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +17,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClientDatabaseStartupSynchronization implements CommandLineRunner {
-    private final JsonParser jsonParser;
+    private static final String NODE_NAME = "results";
+    private final RickAndMortyAPIClient apiClient;
     private final CharacterService characterService;
     private final EpisodeService episodeService;
     private final OriginMapper originMapper;
     private final LocationMapper locationMapper;
     private final CharacterMapper characterMapper;
 
-
     @Autowired
-    public ClientDatabaseStartupSynchronization(JsonParser jsonParser,
+    public ClientDatabaseStartupSynchronization(RickAndMortyAPIClient apiClient,
                                                 CharacterService characterService,
                                                 EpisodeService episodeService,
                                                 OriginMapper originMapper,
                                                 LocationMapper locationMapper,
                                                 CharacterMapper characterMapper) {
-        this.jsonParser = jsonParser;
+        this.apiClient = apiClient;
         this.characterService = characterService;
         this.episodeService = episodeService;
         this.originMapper = originMapper;
@@ -43,9 +42,8 @@ public class ClientDatabaseStartupSynchronization implements CommandLineRunner {
 
     @Override
     public void run(String... args)
-            throws JsonProcessingException, URISyntaxException {
-        CharactersDto charactersDto = jsonParser.parseToPojo();
-        List<CharacterDto> dtos = charactersDto.getCharacterDtos();
+            throws IOException, URISyntaxException {
+        List<CharacterDto> dtos = apiClient.parseJsonToDtoList(NODE_NAME);
         for (CharacterDto dto: dtos) {
             Character character = characterMapper.toModel(dto);
             character.setOrigin(originMapper.toModel(dto.getOrigin()));
