ed2e34cc82afd51c0a854fcd1801070f6d280efd
olehedza <BiewZpo2>
2020-09-10 02:44:13 +0300

    terminated

--- src/main/java/com/olehedza/rickandmorty/service/JsonParser.java
+++
@@ -1,9 +0,0 @@
-package com.olehedza.rickandmorty.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.olehedza.rickandmorty.dto.client.CharactersDto;
-import java.net.URISyntaxException;
-
-public interface JsonParser {
-    CharactersDto parseToPojo() throws JsonProcessingException, URISyntaxException;
-}
