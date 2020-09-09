1ee337f4d1b923b119e43dc7f5e964b88aa97b82
olehedza <BiewZpo2>
2020-09-09 22:37:07 +0300

    annotate field for application property injection and change catch exception block

--- src/main/java/com/olehedza/rickandmorty/client/RickAndMortyAPIClient.java
+++ src/main/java/com/olehedza/rickandmorty/client/RickAndMortyAPIClient.java
@@ -8,40 +8,42 @@ import java.net.URISyntaxException;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RickAndMortyAPIClient {
-    private static final String URI = "https://rickandmortyapi.com/api/character/";
     private static final Logger LOGGER = LoggerFactory
             .getLogger(RickAndMortyAPIClient.class);
     private final RestTemplate restTemplate = new RestTemplate();
+    @Value("${api.character.uri}")
+    private String characterUri;
 
-    public JsonNode getJsonNode(String nodeName) {
-        JsonNode jsonNode = new ObjectMapper().missingNode();
+    public JsonNode getJsonNode(String nodeName)
+            throws JsonProcessingException, URISyntaxException {
+        JsonNode jsonNode;
         try {
             jsonNode = getJson().orElse(new ObjectMapper()
                     .readTree("{}")).path(nodeName);
         } catch (JsonProcessingException e) {
             LOGGER.error("Error while json string processing", e);
+            throw e;
+        } catch (URISyntaxException e) {
+            LOGGER.error("Error while URI string processing", e);
+            throw e;
         }
         return jsonNode;
     }
 
-    private Optional<JsonNode> getJson() {
-        JsonNode root = null;
-        try {
-            ResponseEntity<String> responseEntity = restTemplate
-                    .getForEntity(new URI(URI), String.class);
-            ObjectMapper mapper = new ObjectMapper();
-            root = mapper.readTree(responseEntity.getBody());
-        } catch (URISyntaxException e) {
-            LOGGER.error("Error while URI string processing", e);
-        } catch (JsonProcessingException e) {
-            LOGGER.error("Error while json string processing", e);
-        }
+    private Optional<JsonNode> getJson()
+            throws URISyntaxException, JsonProcessingException {
+        ResponseEntity<String> responseEntity = restTemplate
+                    .getForEntity(new URI(characterUri), String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseEntity.getBody());
+
         return Optional.ofNullable(root);
     }
 }
