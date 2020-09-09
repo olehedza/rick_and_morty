5706e247b4acfe38de448c71585e01ba4691dd0a
olehedza <BiewZpo2>
2020-09-10 02:49:30 +0300

    create one method for json to object parsing

--- src/main/java/com/olehedza/rickandmorty/client/RickAndMortyAPIClient.java
+++ src/main/java/com/olehedza/rickandmorty/client/RickAndMortyAPIClient.java
@@ -1,11 +1,14 @@
 package com.olehedza.rickandmorty.client;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.olehedza.rickandmorty.dto.client.CharacterDto;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,33 +20,37 @@ import org.springframework.web.client.RestTemplate;
 public class RickAndMortyAPIClient {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(RickAndMortyAPIClient.class);
-    private final RestTemplate restTemplate = new RestTemplate();
-    @Value("${api.character.uri}")
-    private String characterUri;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private @Value("${api.character.uri}") String characterUri;
 
-    public JsonNode getJsonNode(String nodeName)
-            throws JsonProcessingException, URISyntaxException {
-        JsonNode jsonNode;
-        try {
-            jsonNode = getJson().orElse(new ObjectMapper()
-                    .readTree("{}")).path(nodeName);
-        } catch (JsonProcessingException e) {
-            LOGGER.error("Error while json string processing", e);
-            throw e;
-        } catch (URISyntaxException e) {
-            LOGGER.error("Error while URI string processing", e);
-            throw e;
-        }
-        return jsonNode;
+    public RickAndMortyAPIClient() {
+        restTemplate = new RestTemplate();
+        objectMapper = new ObjectMapper();
     }
 
-    private Optional<JsonNode> getJson()
-            throws URISyntaxException, JsonProcessingException {
-        ResponseEntity<String> responseEntity = restTemplate
+    public List<CharacterDto> parseJsonToDtoList(String nodeName)
+            throws JsonProcessingException, URISyntaxException {
+        List<CharacterDto> dtoList = new ArrayList<>();
+        try {
+            ResponseEntity<String> responseEntity = restTemplate
                     .getForEntity(new URI(characterUri), String.class);
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(responseEntity.getBody());
+            JsonNode root = objectMapper.readTree(responseEntity.getBody());
+            JsonNode jsonNodes = root.path(nodeName);
 
-        return Optional.ofNullable(root);
+            for (JsonNode node: jsonNodes) {
+                dtoList.add(objectMapper.readValue(node.toString(), CharacterDto.class));
+            }
+        } catch (URISyntaxException e) {
+            LOGGER.error("Error while URI string processing.", e);
+            throw e;
+        } catch (JsonMappingException e) {
+            LOGGER.error("Error while json string mapping.", e);
+            throw e;
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Error while json string processing.", e);
+            throw e;
+        }
+        return dtoList;
     }
 }
